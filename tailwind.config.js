c050c7c97012f6f9cef1103d648996046dd6275e
Andreas Kollegger <akollegger@gmail.com>
2021-01-08 13:52:44 +0100

    revised styling to be fully tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,39 +1,59 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   purge: [
-    './common/*.html',
+    // './common/*.html',
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-        fontFamily: {
-            "sans": ['"Open Sans"', ...defaultTheme.fontFamily.sans],
-            "serif": ['"Cormorant Garamond"', ...defaultTheme.fontFamily.serif],
-            "mono": ['"Fira Code"', ...defaultTheme.fontFamily.mono]
-        },
-        typography: {
-          DEFAULT: {
-            css: {
-              'h1,h2,h3,h4': {
-                fontWeight: 'inherit',
-              },
-              code: {
-                backgroundColor: defaultTheme.colors.gray[100],
-              },
-              'code::before': {
-                content: '""',
-              },
-              'code::after': {
-                content: '""',
-              },
+      fontFamily: {
+        sans: ['"Open Sans"', ...defaultTheme.fontFamily.sans],
+        serif: ['"Cormorant Garamond"', ...defaultTheme.fontFamily.serif],
+        mono: ['"Fira Code"', ...defaultTheme.fontFamily.mono],
+      },
+      typography: {
+        DEFAULT: {
+          css: {
+            "h1,h2,h3,h4": {
+              fontWeight: "inherit",
+            },
+            code: {
+              backgroundColor: defaultTheme.colors.gray[100],
+            },
+            "code::before": {
+              content: '""',
+            },
+            "code::after": {
+              content: '""',
             },
           },
         },
+      },
+
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        blue: {
+          light: "#85d7ff",
+          DEFAULT: "#1fb6ff",
+          dark: "#009eeb",
+        },
+        pink: {
+          light: "#ff7ce5",
+          DEFAULT: "#ff49db",
+          dark: "#ff16d1",
+        },
+        gray: {
+          darkest: "#1f2d3d",
+          dark: "#30333a",
+          DEFAULT: "#45484e",
+          light: "#e0e6ed",
+          lightest: "#f9fafc",
+        },
+      },
     },
   },
   variants: {},
-  plugins: [
-      require('@tailwindcss/typography'),
-  ],
-}
+  plugins: [require("@tailwindcss/typography")],
+};
