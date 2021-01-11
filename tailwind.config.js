50d6a94a8c74e3f74dc62a4d6dcf2918ad5bc36a
Andreas Kollegger <akollegger@gmail.com>
2021-01-11 13:40:59 +0100

    tweaking tailwind prose to only affect within post (not everywhere in UI)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,13 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
+const round = (num) =>
+  num
+    .toFixed(7)
+    .replace(/(\.[0-9]+?)0+$/, '$1')
+    .replace(/\.0$/, '')
+const rem = (px) => `${round(px / 16)}rem`
+const em = (px, base) => `${round(px / base)}em`
+
 module.exports = {
   purge: [
     // './common/*.html',
@@ -15,6 +23,7 @@ module.exports = {
       typography: {
         DEFAULT: {
           css: {
+            maxWidth: "inherit",
             "h1,h2,h3,h4": {
               fontWeight: "inherit",
             },
@@ -27,6 +36,13 @@ module.exports = {
             "code::after": {
               content: '""',
             },
+            'ul': {
+              paddingLeft: '0',
+            },
+            'li': {
+              marginTop: '0',
+              marginBottom: '0'
+            }
           },
         },
       },
